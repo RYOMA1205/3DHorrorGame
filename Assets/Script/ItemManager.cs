36a81b1b31b1b9a717f521b539d122e7d2c6484f
RYOMA1205 <ryoma.take@gmail.com>
2021-09-11 21:26:05 +0900

    コミット アイテムランダム配置実装完了

--- Assets/Script/ItemManager.cs
+++ Assets/Script/ItemManager.cs
@@ -6,6 +6,34 @@ public class ItemManager : MonoBehaviour
 {
     public bool[] haveItems;
 
+    // 型名の最後に[]をつけることで配列にできる
+    public ItemDetail[] itemPrefabs;
+
+    public Transform leftBottomTran;
+    public Transform rightTopTran;
+
+    void Start()
+    {
+        // アイテムをランダムな位置に生成
+        // ここでメソッドを実行する
+        CreateRandomItems();
+    }
+
+    public void CreateRandomItems()
+    {
+        for (int i = 0; i < itemPrefabs.Length; i++)
+        {
+            // ランダムな位置を決める
+            float X = Random.Range(leftBottomTran.position.x, rightTopTran.position.x);
+            float Z = Random.Range(leftBottomTran.position.z, rightTopTran.position.z);
+
+            Vector3 RandomPos = new Vector3(X, 1.2f, Z);
+
+            // ランダムな位置に生成する
+            Instantiate(itemPrefabs[i], RandomPos, Quaternion.identity);
+        }
+    }
+
     // publicのメソッドは外部のスクリプトからも実行できる
     // 今回はFPSControllerでItemを拾ってそのアイテムに
     // アタッチされているItemDetailで設定しているItemTypeを
